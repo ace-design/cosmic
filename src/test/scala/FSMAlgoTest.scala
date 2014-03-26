2263c8fc7805cdad5e6f6d0f0d8afbd36409e81a
Cyril Cecchinel <cyril@cecchinel.fr>
2014-03-26 13:27:29 +0100

    test skeleton for develop algo

--- src/test/scala/FSMAlgoTest.scala
+++ src/test/scala/FSMAlgoTest.scala
@@ -10,12 +10,7 @@ import org.specs2.mutable._
 class FSMAlgoTest extends Specification{
   "A develop algorithm" should {
     "give a correct action list" in {
-      val nodeA = new Node("A")
-      val nodeB = new Node("B")
-      val trans = new Transition(nodeA, nodeB, new TickCondition(3))
 
-      val result = FSMAlgo.develop(trans)
-      val expected = Set[Action](new DeleteTransition(trans), new AddNode("AD0"), new AddNode("AD1"))
     }
   }
 }
