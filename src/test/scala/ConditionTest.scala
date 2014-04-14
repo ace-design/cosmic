97c7ab2f04b3d406de2cf76a7f58c24765d72d53
Cyril Cecchinel <cyril@cecchinel.fr>
2014-04-14 21:58:30 +0200

    Condition test

---
+++ src/test/scala/ConditionTest.scala
@@ -0,0 +1,29 @@
+import fr.unice.modalis.fsm.condition._
+import org.specs2.mutable.Specification
+
+/**
+ * Created by cyrilcecchinel on 14/04/2014.
+ */
+class ConditionTest extends Specification{
+  "A condition" should {
+    "be equals to an other condition (true case)" in {
+      val t1:Condition = new TickCondition(2)
+      val t2:Condition = new TimeCondition("08:00")
+      val t3:Condition = new TrueCondition
+
+      val t1a:Condition = new TickCondition(2)
+      val t2a:Condition = new TimeCondition("08:00")
+      val t3a:Condition = new TrueCondition
+
+      (t1.equals(t1a) mustEqual(true)) && (t2.equals(t2a) mustEqual(true)) && (t3.equals(t3a) mustEqual(true))
+    }
+
+    "be equals to an other condition (false case)" in {
+      val t2:Condition = new TimeCondition("08:00")
+
+      val t3a:Condition = new TrueCondition
+
+      t2.equals(t3a) mustEqual(false)
+    }
+  }
+}
