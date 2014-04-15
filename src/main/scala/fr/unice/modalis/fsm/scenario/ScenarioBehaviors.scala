266badf86a7b7080e26fb42240b008727df7e72e
Cyril Cecchinel <cyril@cecchinel.fr>
2014-04-16 00:58:55 +0200

    Update scenarios

--- src/main/scala/fr/unice/modalis/fsm/scenario/ScenarioBehaviors.scala
+++ src/main/scala/fr/unice/modalis/fsm/scenario/ScenarioBehaviors.scala
@@ -21,8 +21,7 @@ trait Scenario {
 object HeatingMonitoring extends Scenario{
 
  override def init():Behavior = {
-    val n = new Node("TEMP")
-    n.addAction(new EmitAction("AlicePC",8080))
+    val n = new Node("TEMP").addAction(new EmitAction("AlicePC",8080))
     val t = new Transition(n,n, new TickCondition(30))
     new Behavior(n).addTransition(t)
   }
@@ -35,8 +34,7 @@ object HeatingMonitoring extends Scenario{
 object IntrusionPrevention extends Scenario {
 
   override def init(): Behavior = {
-    val n = new Node("WINDOW")
-    n.addAction(new EmitAction("BobPC", 8080).addConstrain(new TimeConstraint("19:00", "06:00")))
+    val n = new Node("WINDOW").addAction(new EmitAction("BobPC", 8080).addConstrain(new TimeConstraint("19:00", "06:00")))
     val t = new Transition(n, n, new TickCondition(10))
     new Behavior(n).addTransition(t)
   }
@@ -50,8 +48,7 @@ object IntrusionPrevention extends Scenario {
 object AirQuality extends Scenario {
 
   override def init(): Behavior = {
-    val n = new Node("ENVIRONMENT")
-    n.addAction(new EmitAction("CharliePC", 8080).addConstrain(new TimeConstraint("19:00", "06:00")))
+    val n = new Node("ENVIRONMENT").addAction(new EmitAction("CharliePC", 8080).addConstrain(new TimeConstraint("19:00", "06:00")))
     val t = new Transition(n, n, new TickCondition(3))
     new Behavior(n).addTransition(t)
   }
@@ -65,8 +62,7 @@ object AirQuality extends Scenario {
 object EnergyLoss extends Scenario {
 
   override def init(): Behavior = {
-    val n = new Node("ENERGY")
-    n.addAction(new EmitAction("DelphinePC", 8080).addConstrain(new TimeConstraint("09:00", "17:00")))
+    val n = new Node("ENERGY").addAction(new EmitAction("DelphinePC", 8080).addConstrain(new TimeConstraint("09:00", "17:00")))
     val t = new Transition(n, n, new TickCondition(60))
     new Behavior(n).addTransition(t)
   }
@@ -80,8 +76,7 @@ object EnergyLoss extends Scenario {
 object CarPooling extends Scenario {
 
   override def init(): Behavior = {
-    val n = new Node("PARKING")
-    n.addAction(new EmitAction("Etienne", 8080).addConstrain(new TimeConstraint("08:00", "09:00")).addConstrain(new TimeConstraint("17:00", "18:00")))
+    val n = new Node("PARKING").addAction(new EmitAction("Etienne", 8080).addConstrain(new TimeConstraint("08:00", "09:00")).addConstrain(new TimeConstraint("17:00", "18:00")))
     val t = new Transition(n, n, new TickCondition(2))
     new Behavior(n).addTransition(t)
   }
@@ -95,8 +90,7 @@ object CarPooling extends Scenario {
 object WindowOpening extends Scenario {
 
   override def init(): Behavior = {
-    val n = new Node("OPENING")
-    n.addAction(new EmitAction("Francois", 8080).addConstrain(new TimeConstraint("09:00", "14:00")))
+    val n = new Node("OPENING").addAction(new EmitAction("Francois", 8080).addConstrain(new TimeConstraint("09:00", "14:00")))
     val t = new Transition(n, n, new TickCondition(60))
     new Behavior(n).addTransition(t)
   }
